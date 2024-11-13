51eee5f18018842262717e0eaf5acaff0d86b87b
paulohenrique19 <paulo170000@gmail.com>
2024-11-13 17:55:52 -0300

    13/11/24

--- javascript/variaveis_e_constantes.js
+++ javascript/variaveis_e_constantes.js
@@ -11,3 +11,39 @@ const permite que a variável seja declarada uma vez, com escopo de bloco, e nã
 
 
 */
+
+/*Usar `` permite escrever multiplas linhas */
+
+let a = "TESTE"
+
+console.log(`
+    
+    TESTE
+    TESTE
+    TESTE
+    ${a}
+
+    `)
+
+/* Realizar operações entre números e não números pode retornar um valor como "NaN": not a number */
+
+
+/*Conversão de tipos */
+
+console.log(true)
+console.log(false)
+console.log(False)
+console.log(typeof Number("9"))
+console.log(typeof String(9))
+
+/*.toString() e String() converte para texto 
+  Number() converte para numero
+  Boolean() converte para booleano
+    */
+
+
+/*Operador ternário */
+
+let b = 10
+
+console.log(b >= 10 ? "maior/igual a 10" : "menor que 10")
