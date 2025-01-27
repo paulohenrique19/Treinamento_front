8a1daeaca32603c267286d0b56e79c618b538e35
paulohenrique19 <paulo170000@gmail.com>
2025-01-27 17:57:18 -0300

    27/01/25

--- react-com-typescript/use-form-class/src/App.tsx
+++ react-com-typescript/use-form-class/src/App.tsx
@@ -1,33 +1,86 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+// o zod é usado para validação
+const createUserFormSchema = z.object({
+  name: z.string()
+    .nonempty('O nome é obrigatório')
+    .transform(name => { //transform() aplica uma transformação PERSONALIZADA no objeto
+      return name.trim().split(' ').map(word => {
+        return word[0].toLocaleUpperCase().concat(word.substring(1))
+      }).join(' ')
+    }), 
+  email: z.string()
+    .nonempty('O e-mail é obrigatório')
+    .email('Formato de e-mail inválido')
+    .toLowerCase(),
+  password: z.string()
+    .min(6, 'Password must be at least 6 characters long')
+})
+
+//criando tipagem para os dados e inferindo o tipo com base no tipo do createUserFormSchema
+type CreateUserFormData = z.infer<typeof createUserFormSchema>
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [ output, setOutput ] = useState('')
+  const { 
+    register, 
+    handleSubmit, 
+    formState: { errors } 
+  } = useForm<CreateUserFormData>({
+    resolver: zodResolver(createUserFormSchema)
+  })
+  
+  
+  /*register serve pra registrar os campos nos inputs
+    usando register, não precisamos mais do atributo 'name'
+  */
+
+  function createUser(data: any) {
+    setOutput(JSON.stringify(data, null, 2))
+  }
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <main>
+          <form onSubmit={handleSubmit(createUser)} action="">
+            <div>
+              <label htmlFor="name">Nome</label>
+              <input 
+                type="text" 
+                {...register('name')}
+              />
+              {errors.name && <span>{errors.name.message}</span>}
+            </div>
+            
+            
+            <div>
+              <label htmlFor="email">Email</label>
+              <input 
+                type="email" 
+                {...register('email')}
+              />
+              {errors.email && <span>{errors.email.message}</span>}
+            </div>
+
+            <div>
+              <label htmlFor="password">Senha</label>
+              <input 
+                type="password" 
+                {...register('password')}
+              />
+              {errors.password && <span>{errors.password.message}</span>}
+            </div>
+
+            <button type='submit'>Salvar</button>
+          </form>
+          <pre>{output}</pre>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
