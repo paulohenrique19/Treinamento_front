8eda5cb58ff2f91b3b779c61777802913ae330e0
paulohenrique19 <paulo170000@gmail.com>
2025-03-11 17:51:33 -0300

    11/03/25

--- react-com-typescript/4_projeto2/src/pages/End/index.tsx
+++ react-com-typescript/4_projeto2/src/pages/End/index.tsx
@@ -1,48 +1,50 @@
-import { Container, Data, Separator, Image } from "./styles";
+import { Container, Data, Separator, Image, LocalIconEnd, ClockIconEnd, CashIconEnd, Icon } from "./styles";
 
 export const End = () => {
     const endereco = JSON.parse(localStorage.getItem("endereco") || "{}");
     
-
     const typePagament = (value: any) => {
+        if (value === 1) {
+            return "Dinheiro";
+        }
+        if (value === 2) {
+            return "Cartão Débito/Crédito";
+        }
+        if (value === 3) {
+            return "PIX";
+        }
+    };
 
-        let result = null
-
-        if (value === 1)
-        {
-            return (
-                result = "Dinheiro"
-            )
-        }
-        if (value === 2)
-        {
-            return (
-                result = "Cartão Débito/Crédito"
-            )
-        }
-        if (value === 3)
-        {
-            return (
-                result = "PIX"
-            )
-        }
-    }
-    
     return (
       <Container>
         <h2>Endereço de Entrega</h2>
         <Separator>
             <Data>
-                <p>Entrega em: <strong> {endereco.rua}</strong></p>
-                <p><strong> {endereco.cidade}</strong></p>
-                <p>CEP: <strong> {endereco.cep}</strong></p>
-                <p>Forma de pagamento: <strong>{typePagament(endereco.formaPagamento)}</strong></p>
+                <LocalIconEnd>
+                    <Icon src="src/assets/images/End/LocalIconEnd.svg" alt="Ícone Local" />
+                    <div>
+                        <p>Entrega em: <strong>{endereco.rua}</strong></p>
+                        <p><strong>{endereco.cidade}</strong></p>
+                    </div>
+                </LocalIconEnd>
+                <ClockIconEnd>
+                    <Icon src="src/assets/images/End/ClockIconEnd.svg" alt="Ícone Relógio" />
+                    <div>
+                        <p>Previsão de entrega</p>
+                        <p><strong>20 min - 30 min</strong></p>
+                    </div>
+                </ClockIconEnd>
+                <CashIconEnd>
+                    <Icon src="src/assets/images/End/CashIconEnd.svg" alt="Ícone Dinheiro" />
+                    <div>
+                        <p>Forma de pagamento: <strong>{typePagament(endereco.formaPagamento)}</strong></p>
+                    </div>
+                </CashIconEnd>
             </Data>
             <Image>
-                <img src="src/assets/images/End/Motorcycle.svg" alt="" />
+                <img src="src/assets/images/End/Motorcycle.svg" alt="Moto" />
             </Image>
         </Separator>
       </Container>
     );
-  };
-  
+};
--- react-com-typescript/4_projeto2/src/pages/End/styles.tsx
+++ react-com-typescript/4_projeto2/src/pages/End/styles.tsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 
+
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
@@ -11,6 +12,7 @@ export const Container = styled.div`
   padding: 2rem;
 `;
 
+
 export const Separator = styled.div`
   display: flex;
   flex-direction: row;
@@ -19,25 +21,46 @@ export const Separator = styled.div`
   max-width: 80rem;
   margin: 0 auto;
   align-items: center;
-  justify-content: space-between;
-  gap: 21rem
+  gap: 21rem;
 `;
 
-export const Image = styled.div`
 
-`
+export const Image = styled.div``;
+
 
 export const Data = styled.div`
- display: flex;
- flex-direction: column;
- width: auto;
- gap: 1rem;
- padding: 2.5rem;
- min-width: 26.5rem;
- border: 3px solid transparent;  /* Transparente para a borda */
- border-image:  linear-gradient(90deg, rgba(252,176,69,1) 0%, rgba(158,116,230,1) 100%);
- border-image-slice: 1;  /* Necessário para o gradiente preencher a borda */
- 
+  display: flex;
+  flex-direction: column;
+  width: auto;
+  gap: 2.5rem;
+  padding: 2.5rem;
+  min-width: 26.5rem;
+  border: 3px solid transparent;
+  border-image: linear-gradient(90deg, rgba(252,176,69,1) 0%, rgba(158,116,230,1) 100%);
+  border-image-slice: 1;
+`;
 
-`
+export const LocalIconEnd = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const ClockIconEnd = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+export const CashIconEnd = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
+
+
+export const Icon = styled.img`
+  width: 28px; 
+  height: 28px; 
+`;
 
