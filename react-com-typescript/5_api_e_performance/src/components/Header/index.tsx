cdb05d07ca954a4b1c8f73e0dde1ab1f0a87d78f
paulohenrique19 <paulo170000@gmail.com>
2025-03-17 17:53:47 -0300

    17/03/25

--- react-com-typescript/5_api_e_performance/src/components/Header/index.tsx
+++ react-com-typescript/5_api_e_performance/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import logoImg from '../../assets/logo.svg' 
+import * as Dialog from "@radix-ui/react-dialog";
+import { NewTransactionModel } from "../NewTransactionModal";
 
 
 export function Header() {
@@ -7,8 +9,17 @@ export function Header() {
         <HeaderContainer>
             <HeaderContent>
                 <img src={logoImg} alt="" />
-                <NewTransactionButton>Nova transação</NewTransactionButton>
+
+                {/**Criando modal usando radix-ui*/}
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <NewTransactionButton>Nova transação</NewTransactionButton>
+                    </Dialog.Trigger>
+                    
+                    <NewTransactionModel />
+                </Dialog.Root>                
             </HeaderContent>
         </HeaderContainer>
     )
 }
+
--- react-com-typescript/5_api_e_performance/src/pages/Transactions/index.tsx
+++ react-com-typescript/5_api_e_performance/src/pages/Transactions/index.tsx
@@ -1,11 +1,43 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+
+import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
     return (
         <div>
             <Header />
             <Summary />
+
+            <TransactionsContainer>
+                <SearchForm />
+            
+                <TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento do site</td>
+                            <td>
+                                <PriceHighLight variant="income">
+                                    R$ 12.000,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>13/04/2022</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Impostos</td>
+                            <td>
+                                <PriceHighLight variant="outcome">
+                                    - R$ 50,00
+                                </PriceHighLight>
+                            </td>
+                            <td>Taxas</td>
+                            <td>14/04/2022</td>
+                        </tr>
+                    </tbody>
+                </TransactionsTable>
+            </TransactionsContainer>
         </div>
     )
 }
