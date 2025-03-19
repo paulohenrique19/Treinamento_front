9938c34ca29a4582a5100b11a93f977f9989026b
paulohenrique19 <paulo170000@gmail.com>
2025-03-19 17:51:30 -0300

    19/03/25

--- react-com-typescript/5_api_e_performance/src/components/NewTransactionModal/index.tsx
+++ react-com-typescript/5_api_e_performance/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import * as Dialog from "@radix-ui/react-dialog";
+import * as RadioGroup from "@radix-ui/react-radio-group";
 import { CloseButton, 
         Content, 
         Overlay, 
@@ -26,12 +27,12 @@ export function NewTransactionModel() {
                     <input type="text" placeholder="Categoria" required/>
 
                     <TransactionType>
-                        <TransactionTypeButton variant="income">
+                        <TransactionTypeButton variant="income" value="income">
                             <ArrowCircleUp size={24}/>
                             Entrada
                         </TransactionTypeButton>
 
-                        <TransactionTypeButton variant="outcome">
+                        <TransactionTypeButton variant="outcome" value="outcome">
                             <ArrowCircleDown size={24}/>
                             Saída
                         </TransactionTypeButton>
